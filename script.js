a4b08e36357311930500970ab1803a4a7254f55d
Somalika-1 <arorasomalika@gmail.com>
2025-03-22 23:37:22 +0530

    new update 22

--- script.js
+++ script.js
@@ -109,6 +109,15 @@ async function main() {
         currentsong.currentTime=(currentsong.duration*percent)/100
     })
 
+    //add an event listener for hamburger
+    document.querySelector(".hamburger").addEventListener("click",()=>{
+        document.querySelector(".left").style.left="0"
+    })
+
+    //add an event listener for close button
+    document.querySelector(".close").addEventListener("click",()=>{
+        document.querySelector(".left").style.left="-130%"
+    })    
 }
 
 main()
